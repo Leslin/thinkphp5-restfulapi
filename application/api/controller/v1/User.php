8a95471ab5660520d800dfede42d6ae3b4dc9647
dalin <li@cdhand.com>
2018-01-30 17:56:54 +0800

    debug
    debug

--- application/api/controller/v1/User.php
+++ application/api/controller/v1/User.php
@@ -13,7 +13,6 @@ use app\api\controller\UnauthorizedException;
  * 基类控制器提供了基础的验证，包含app_token,请求时间，请求是否合法的一系列的验证
  * 在所有子类中可以调用$this->clientInfo对象访问请求客户端信息，返回为一个数组
  * 在具体资源方法中，不需要再依赖注入，直接调用$this->request返回为请具体信息的一个对象
- * date:2017-07-25
  */
 class User extends Api
 {   
