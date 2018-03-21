23a4d11202017170086d816dbed90047c8a1f4ee
dalin <li@cdhand.com>
2018-03-21 09:43:45 +0800

    debug
    debug

--- application/route.php
+++ application/route.php
@@ -10,9 +10,9 @@
 // +----------------------------------------------------------------------
 use think\Route;
 
-Route::resource(':version/user','api/:version.User');   //注册一个资源路由，对应restful各个方法
+Route::resource(':version/user','api/:version.User');   //注册一个资源路由，对应restful各个方法,.为目录
 
-Route::rule(':version/user/:id/fans','api/:version.User/fans'); //restful方法中另外一个方法等。。。
+Route::rule(':version/user/:id/fans','api/:version.User/fans'); //restful方法中除restful api外的其他方法路由
 Route::rule(':version/token/wechat','api/:version.Token/wechat');
 Route::rule(':version/token/mobile','api/:version.Token/mobile');
 Route::miss('Error/index');
