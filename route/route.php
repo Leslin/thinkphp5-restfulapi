ea075fa47f5cdd12f2ecf5bcfd8ef02f6dcd21bd
GeeHon <liaoliaoliao70@gmail.com>
2021-05-08 11:57:59 +0800

    修正错误import

--- route/route.php
+++ route/route.php
@@ -8,13 +8,15 @@
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
-use think\Route;
-//一般路由规则，访问的url为：v1/address/1,对应的文件为Address类下的read方法
-Route::get(':version/address/:id','api/:version.user/address');
 
-Route::resource(':version/user','api/:version.user');       //资源路由，详情查看tp手册资源路由一章
+use think\facade\Route;
+
+//一般路由规则，访问的url为：v1/address/1,对应的文件为Address类下的read方法
+Route::get(':version/address/:id', 'api/:version.user/address');
+
+Route::resource(':version/user', 'api/:version.user');       //资源路由，详情查看tp手册资源路由一章
 
 //生成access_token，post访问Token类下的token方法
-Route::post(':version/token','api/:version.token/token');
-Route::post(':version/token/refresh','api/:version.token/refresh');
+Route::post(':version/token', 'api/:version.token/token');
+Route::post(':version/token/refresh', 'api/:version.token/refresh');
 
